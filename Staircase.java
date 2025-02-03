687f5884166bb2b3cf25d69829ef51eb58a98e60
Nikhil4123 <nikhilsarak612w@gmail.com>
2025-02-03 22:37:29 +0530

    java Staircase

---
+++ Staircase.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+class Staircase {
+
+    /*
+     * Complete the 'staircase' function below.
+     *
+     * The function accepts INTEGER n as a parameter.
+     */
+    public static void staircase(int n) {
+        for (int i = 1; i <= n; i++) {
+            // Print spaces
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+            // Print hashes
+            for (int j = 0; j < i; j++) {
+                System.out.print("#");
+            }
+            System.out.println(); // Move to the next line
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        testCases();
+    }
+
+    public static void testCases() {
+        System.out.println("Test Case 1: n = 1");
+        staircase(1);
+        System.out.println("\nTest Case 2: n = 3");
+        staircase(3);
+        System.out.println("\nTest Case 3: n = 5");
+        staircase(5);
+    }
+}
